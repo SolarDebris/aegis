8fa807ac0a3227f203804b130b6c160d5c4d6921
Alex Schmith <alexanderschmith@protonmail.com>
2022-12-30 21:12:13 -0500

    cleaned up code a bit

--- rage/machine.py
+++ rage/machine.py
@@ -1,25 +1,17 @@
-import sys
-import os
 import subprocess
 import ropgadget
 import argparse
 import logging
+import pwn
 
 import binaryninja as bn
 
-from pwn import *
-
-# ------------------------------------------------------------------------------------------#
-#                                                                                           #
-#                              Machine Static Analysis                                      #
-#                                                                                           #
-# ------------------------------------------------------------------------------------------#
-
-
-# This class is strictly for getting things out of the binary through static
-# analysis and and other analysis stuff
 class Machine:
+    """Class that is used for static analysis of a binary and grabbing
+    other information out of a binary"""
+
     def __init__(self, binary):
+        """Set up all variables for the class."""
         self.binary = binary
         self.bv = bn.open_view(self.binary)
         self.arch = self.bv.arch.name
@@ -41,20 +33,18 @@ class Machine:
         self.buffer_overflow = False
         self.format_vuln = False
 
-
-    # Function that returns whether or not a variable is user controlled through either
-    # argument, input, or a file
     def is_user_controlled(self, variable: bn.variable.Variable):
+        """
+        Return true or false if the variable is user controlled.
+
+        Check if a variable is user controlled through arguments or prompted
+        user input.
+        """
         return None
 
-
-    #-----------------------------------------------------------------------------#
-    #                               Checks                                        #
-    #-----------------------------------------------------------------------------#
-
-    # Function that gets all the mitigations of the binary
     def check_mitigations(self):
-        elf = ELF(self.binary)
+        """Return all of the mitigations of the binary."""
+        elf = pwn.ELF(self.binary)
 
         self.aslr = elf.aslr
         self.canary = elf.canary
@@ -62,22 +52,32 @@ class Machine:
         self.pie = elf.pie
         self.relro = elf.relro
 
-
-    # Returns if there is an overflow with the given medium level il instruction
     def check_overflow(self, instruction: bn.mediumlevelil.MediumLevelILCall):
+        """
+        Return true or false if there is a stack based buffer overflow.
+
+        Check for buffer overflow given a medium level il instruction call.
+        """
         function = self.bv.get_functions_containing(instruction.address)[0]
         dest_function = self.bv.get_function_at(instruction.dest.constant)
         var = None
         buff = None
 
-
         # If function is reading in something to a variable,
         # we check the space the stack has before and make sure that the
         # input is less than or equal to the space the stack has left befor
-        if "gets" in dest_function.name or "read" in dest_function.name:
+        if "gets" in dest_function.name or "read" in dest_function.name or "scanf" in dest_function.name:
+            input_size = 0
             # Get the variable name that is in the input
+            if dest_function.name == "__isoc99_scanf":
+                buff = instruction.get_reg_value(self.reg_args[1])
+                # !TODO Check if variable is on the stack
+                # !TODO Check if format specifier is %s
+                input_size = 10000
+
             if dest_function.name == "fgets":
                 buff = instruction.get_reg_value(self.reg_args[0])
+
                 input_size = instruction.params[1].constant
 
             elif dest_function.name == "gets":
@@ -94,32 +94,25 @@ class Machine:
             self.padding_size = self.get_padding_size(function, var)
             if self.padding_size  < input_size:
                 print(f"Found buffer overflow with a padding of {self.padding_size}")
-                if self.canary:
-                    self.padding_size -= 16
                 self.buffer_overflow = True
-
 
         # !TODO Get buffer size for vulnerable memcpy/strcpy
         elif "cpy" in dest_function.name:
             input_var = None
             output_var = None
 
-
-    # This function checks if a given function is a win function
     def check_win_function(self, function: bn.function.Function):
+        """Check if the current function qualifies as a win function."""
         return None
 
-    # This function will find a vulnerable printf in which the format specifier
-    # is a stack value and that the vargs is undetermined
     def check_vulnerable_printf(self):
+        """Find a printf that is vulnerable to format string exploits."""
         addresses = []
         for function in self.functions:
             for block in function.medium_level_il:
                 for instruction in block:
                     if instruction.operation == bn.MediumLevelILOperation.MLIL_CALL:
                         if type(instruction.dest) == bn.mediumlevelil.MediumLevelILConstPtr:
-                            format_specifier = instruction.get_reg_value(self.reg_args[0])
-                            vargs = instruction.get_reg_value(self.reg_args[1])
                             if len(instruction.params) > 0 and type(instruction.params[0]) == bn.mediumlevelil.MediumLevelILVar:
                                 #!TODO Check to see if variable can store user data
                                 name = self.bv.get_symbol_at(instruction.dest.constant).name
@@ -129,9 +122,8 @@ class Machine:
                                     addresses.append(instruction.address)
         return addresses
 
-
-    # This function will find vulnerable copies like strcpy, strncpy, and memcpy
     def check_vulnerable_copy(self):
+        """Find vulnerable copy instructions."""
         for function in self.functions:
             for block in function.medium_level_il:
                 for instruction in block:
@@ -140,9 +132,8 @@ class Machine:
                         if name == "memcpy" or name == "strcpy" or name == "strncpy":
                             self.check_overflow(instruction)
 
-
-    # Function that checks for standard input functions and runs check overflow on them
     def check_vulnerable_input(self):
+        """Check for stack overflow in user input functions."""
         # variable for how much stdin has inputted so far
         for function in self.functions:
             for block in function.medium_level_il:
@@ -150,25 +141,16 @@ class Machine:
                     if instruction.operation == bn.MediumLevelILOperation.MLIL_CALL:
                         if type(instruction.dest) == bn.mediumlevelil.MediumLevelILConstPtr:
                             name = self.bv.get_symbol_at(instruction.dest.constant).name
-                            if name == "gets" or name == "fgets" or name == "read":
+                            if name == "gets" or name == "fgets" or name == "read" or "scanf" in name:
                                 self.check_overflow(instruction)
 
-
-    # Returns the size of a buffer in a certain function not including the base pointer and the instruction
-    # pointer
-    def get_function_buffer_size(self, function: bn.function.Function):
-        size = 0
-        for variable, stack_variable in zip(function.core_var_stack_layout, function.stack_layout):
-            if stack_variable.name == "__saved_rbp":
-                print(f"The buffer size for function {function.name} is {size}")
-                return size
-            size -= variable.storage
-
-        return size
-
-
-    # Function to get the padding size for a buffer overflow
     def get_padding_size(self, function: bn.function.Function, input_variable):
+        """
+        Return padding size for buffer overflow.
+
+        Get padding size for variable that's on the stack for overwriting the
+        instruction pointer or canary.
+        """
         size = 0
         variable_set = False
         # Wait until stack reaches the variable
@@ -176,15 +158,17 @@ class Machine:
             if stack_variable.name == input_variable:
                 variable_set = True
             if variable_set:
+                # If there is a canary find subtract the space the canary takes up
+                if self.canary_var != None and stack_variable.name == self.canary_var.name:
+                    size += variable.storage
+                    return size
                 if stack_variable.name == "__saved_rbp":
                     return size
                 size -= variable.storage
         return size
 
-
-    # This function will go through the symbol table and the plt to find
-    # functions that are specified in a list
     def find_functions(self, functions_list):
+        """Return a list of functions in the binary using the given list."""
         funcs = []
         for function in self.functions:
             for func in functions_list:
@@ -193,10 +177,13 @@ class Machine:
 
         return funcs
 
-
-    # This function will return a writable address in the binary without
-    # interfering with variables
     def find_writable_address(self):
+        """
+        Return a writable address in memory.
+
+        Finds a writable address of memory in the binary without interfering
+        with variables.
+        """
         #!TODO Add a section to find an empty set of characters
         for section in self.sections.keys():
             section = self.sections.get(section)
@@ -206,20 +193,19 @@ class Machine:
                     return section.start
         return None
 
-
-    # Returns names of functions in the got that can be overwritten after a certain address
-    def find_got_function_calls(self,  function: bn.function.Function):
-        for block in function.medium_level_il:
-            for instruction in block:
-                if instruction.operation == bn.MediumLevelILOperation.MLIL_CALL:
-                    if type(instruction.dest) == bn.mediumlevelil.MediumLevelILConstPtr:
-                        #!TODO implement a range check in the got and find
-                        name = self.bv.get_symbol_at(instruction.dest.constant).name
+    def find_unused_got_functions(self, address):
+        """Return functions that's got entry is empty at a certain point."""
+        for function in self.functions:
+            for block in function.medium_level_il:
+                for instruction in block:
+                    if instruction.operation == bn.MediumLevelILOperation.MLIL_CALL:
+                        if type(instruction.dest) == bn.mediumlevelil.MediumLevelILConstPtr:
+                            #!TODO implement a range check in the got and find
+                            name = self.bv.get_symbol_at(instruction.dest.constant).name
         return None
 
-
-    # Return the address of an important string being /bin/sh, cat flag.txt, or flag.txt
     def find_string_address(self):
+        """Return the address of a string used to get flag."""
         important_strings = ["/bin/sh", "/bin/cat flag.txt", "cat flag.txt", "flag.txt"]
 
         for string in self.bv.strings:
@@ -231,9 +217,8 @@ class Machine:
                     return address
         return None
 
-
-    # Returns the address of system("/bin/sh") or any other one_gadget
     def find_win_gadget(self):
+        """Return the address of a win gadget."""
         important_strings = ["/bin/sh", "/bin/cat flag.txt", "cat flag.txt"]
         functions = ["system", "execve"]
 
@@ -251,9 +236,8 @@ class Machine:
                         return address
         return None
 
-
-    # Return a pop register gadget with the least amount of instructions
     def find_pop_reg_gadget(self, register):
+        """Return a rop gadget to control a register using a pop gadget."""
         output = subprocess.check_output(["ROPgadget", "--binary", self.binary, "--re", f"{register}", "--only", "pop|ret"]).split(b"\n")
         output.pop(0)
         output.pop(0)
@@ -280,9 +264,8 @@ class Machine:
         print(f"Found gadget for {register}: {min_gadget}")
         return min_gadget
 
-
-    # Return a mov register gadget with the least amount of instructions that is valid
     def find_mov_reg_gadget(self, register):
+        """Return a rop gadget to control a register using the mov instruction."""
         output = subprocess.check_output(["ROPgadget", "--binary", self.binary, "--re", f"mov {register}" ]).split(b"\n")
         output.pop(0)
         output.pop(0)
@@ -337,9 +320,8 @@ class Machine:
         reg2 = min_gadget.split(b"[")[1].split(b",")[1].split(b"]")[0].split(b";")[0].strip()
         return min_gadget, reg2
 
-
-    # Return a write primitive gadget with priority of it being 64 bit registers
     def find_write_gadget(self):
+        """Return a write primitive rop gadget."""
         output = subprocess.check_output(["ROPgadget", "--binary", self.binary, "--re", "mov .word ptr \[.*\], *.", "--filter", "jmp"]).split(b"\n")
         output.pop(0)
         output.pop(0)
@@ -369,7 +351,6 @@ class Machine:
         # If there are no optimal gadgets choose from valid ones
         if len(optimal_gadgets) <= 0:
             if len(valid_gadgets) <= 0:
-                print("Couldn't find write gadget")
                 return None
             optimal_gadgets = valid_gadgets
 
@@ -388,12 +369,12 @@ class Machine:
         reg2 = min_gadget.split(b"[")[1].split(b",")[1].split(b"]")[0].split(b";")[0].strip()
         return min_gadget, reg1, reg2
 
-
-    # Function that renames the binary ninja analysis to make things easier
-    def rename_analysis(self):
-        # If there is a canary find the function that uses stack_check_fail and rename the canary variable on the stack
+    def rename_analysis(self):        """Function that will rename variables and functions in order to make
+        analysis easier"""
+        # If there is a canary find the function that uses stack_check_fail
+        # and rename the canary variable on the stack
+        self.canary_var = None
         if self.canary:
-            canary_function = None
             for function in self.functions:
                 for block in function.medium_level_il:
                     for instruction in block:
@@ -403,28 +384,20 @@ class Machine:
                                 if name == "__stack_chk_fail":
                                     variables = function.stack_layout
                                     canary_variable = variables[-3]
+                                    self.canary_var = canary_variable
                                     print(canary_variable.name)
-                                    offset = canary_variable.storage
-                                    function.create_user_stack_var(offset, canary_variable.type, "canary")
-                                    #canary_variable.set_name_async("canary")
+                                    # !FIXME This doesn't work in commercial on
+                                    # linux for some reason
+                                    canary_variable.set_name_async("canary")
                                     print(canary_variable.name)
                                     print(function.stack_layout)
 
-
-
-
         return None
-
-
-
-
 
 
 if __name__ == "__main__":
 
     logging.getLogger("pwnlib").setLevel(logging.CRITICAL)
-
-
 
     parser = argparse.ArgumentParser(
         prog="machine",
