c93c288a61863a18206901df56f86af9c33cf172
Alex Schmith <alexanderschmith@protonmail.com>
2024-11-16 14:31:10 -0500

    trying some stuff

--- rage/poker.py
+++ rage/poker.py
@@ -1,4 +1,7 @@
 from rage.log import aegis_log
+#from rage.machine import Machine
+
+import yaml
 
 """
 This is the class that will generate
@@ -6,4 +9,19 @@ a formula on what steps are needed to pwn.
 """
 class Poker:
 
+    """
+    This will take in a yaml object and be
+    used in the poker class.
+    """
+    self.formula = yaml.load(open('formula.yaml'))
+
     def __init__(self):
+        self.formula = yaml.load(open('formula.yaml'))
+
+
+
+if __name__ == "__main__":
+
+    poker = Poker()
+    print(poker.formula)
+
